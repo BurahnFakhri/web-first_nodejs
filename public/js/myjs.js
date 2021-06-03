8a85601eea79e9ea2142f495edfaa6f8829a7a7c
*Burhan <burhanfakhri53@gmail.com>
2021-06-03 16:28:42 +0530

    =Remove Geocode Commented Code

--- public/js/myjs.js
+++ public/js/myjs.js
@@ -1,9 +1,3 @@
-// fetch('https://api.mapbox.com/geocoding/v5/mapbox.places/Indore.json?access_token=pk.eyJ1IjoiYnVyYWhuIiwiYSI6ImNrcDd6bXQ3bTA1Y3cyb254bG9qNTVoNXUifQ.YIyUiJm-HPAbLjP6ooXH6A&limit=1').then((response)=>{
-//     response.json().then((data)=>{
-//         console.log(data);
-//     });
-// });
-
 fetch('http://localhost:3000/weather?address=!').then((response)=>{
     response.json().then((data)=>{
         console.log(data);
